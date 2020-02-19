aa44b3186db2bb36b9f57c25b3554fe01bb2acb5
itou-t <solanina5775@icloud.com>
2020-02-19 23:23:58 +0900

    second_commit

--- get_follow.py
+++ get_follow.py
@@ -2,14 +2,14 @@ import tweepy
 cursor = -1
 while cursor != 0:
     auth = tweepy.OAuthHandler("2oNzJD7rceT2iWxhKxR9tSZPm", 'zGCwDZ5ihlix0FFCEDJK5Yfza9j4IGm4uyQKOWpueakbBGoz1T')
-    auth.set_access_token('922767128016560129-hVgHBjb4xLTUonC4JEDE3dvY1sjMHQO','a54v2Eu19VucSWph3QyEekTI4kxpoClCKWD9tsTzCTnOG')
+    auth.set_access_token('922767128016560129-JLi7KH5cUX52a8kJC8j5Bd8CJnPrPdY','3cakJdDZGkdomVE3B5jmeUMp6xZlCElBfvlmTmeKvcUyi')
     api = tweepy.API(auth, wait_on_rate_limit=True)
-    itr = tweepy.Cursor(api.friends_ids, id='karasukoee', cursor=cursor).pages()
+    itr = tweepy.Cursor(api.friends_ids, id=input("＠以降のユーザー名を入力してください:"), cursor=cursor).pages()
     try:
         for user_id in itr.next():
             try:
                 user = api.get_user(user_id)
-                user_info = [user.id_str, user.screen_name, user.name,user.following,user.followers_count,]
+                user_info = [user.id_str, user.screen_name, user.name,user.following,user.friends_count,user.followers_count,]
                 print(user_info)
             except tweepy.error.TweepError as e:
                 print(e.reason)
