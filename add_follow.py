2f86a67b34e604e19b3f7f50eed6d633bb05a842
itou-t <60230580+itou-t@users.noreply.github.com>
2020-04-23 00:13:32 +0900

    Update add_follow.py

--- add_follow.py
+++ add_follow.py
@@ -1,29 +1,27 @@
 import tweepy
 import json
 import MySQLdb
-from flask import Flask, redirect, request
 from db import conn_f
+from contextlib import closing
 
 # 認証キーの設定
-def add_follow(ck,cs):
-    random_key = request.form.get('random_key')
-    conn = conn_f()
-    cursor = conn.cursor()
-    sql = 'SELECT access_token, access_token_secret FROM auth WHERE random_key = %s'
-    cursor.execute(sql,(random_key,))
-    token = cursor.fetchone()
-    cursor.close()
-    conn.close()
+def add_follow(ck,cs,random_key,screen_name):
+
+    with closing(conn_f()) as conn:
+        with conn.cursor() as cursor:
+            sql = 'SELECT access_token, access_token_secret FROM auth WHERE random_key = %s'
+            cursor.execute(sql,(random_key,))
+            token = cursor.fetchone()
     access_token = token[0]
     access_token_secret = token[1]
     # OAuth認証
     auth = tweepy.OAuthHandler(ck,cs)
     auth.set_access_token(access_token, access_token_secret)
-    screen_name = request.form.get('screen_name')
+
     # APIのインスタンスを生成
     api = tweepy.API(auth)
     try:
         api.create_friendship(screen_name)
-        return json.dumps({'status_code': 200})
+        return json.dumps({'status_code': 200 , 'message':'OK'})
     except tweepy.error.TweepError:
-        return json.dumps({'status_code':404})
+        return json.dumps({'status_code': 404 , 'message':'Error'})
