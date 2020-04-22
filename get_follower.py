573a6be7cd23bd322e99f0d1faf1659ffb819d77
itou-t <60230580+itou-t@users.noreply.github.com>
2020-04-23 00:19:17 +0900

    Update get_follower.py

--- get_follower.py
+++ get_follower.py
@@ -1,27 +1,25 @@
 import tweepy
 import json
 import MySQLdb
-from flask import Flask, redirect, request
 from db import conn_f
+from contextlib import closing
 
-def get_follower(ck,cs):
-    random_key = request.args.get('random_key','')
-    conn = conn_f()
-    cursor = conn.cursor()
-    sql = 'SELECT access_token, access_token_secret,screen_name FROM auth WHERE random_key=%s'
-    cursor.execute(sql,(random_key,))
-    token = cursor.fetchone()
-    cursor.close()
-    conn.close()
+def get_follower(ck,cs,random_key):
+
+    with closing(conn_f()) as conn:
+        with conn.cursor() as cursor:
+            sql = 'SELECT access_token,access_token_secret,screen_name FROM auth WHERE random_key= %s'
+            cursor.execute(sql,(random_key,))
+            token = cursor.fetchone()
     access_token = token[0]
     access_token_secret = token[1]
     screen_name = token[2]
-    user_info_keys = ["id", "name", "screen_name", "description", "friends_count", "followers_count","following","profile_image_url_https",]
+    user_info_keys = ['id', 'name', 'screen_name', 'description', 'friends_count', 'followers_count','following','profile_image_url_https',]
     cursor = -1
     while cursor != 0:
-        #OAuthHandlerインスタンスの作成
+        # OAuthHandlerインスタンスの作成
         auth = tweepy.OAuthHandler(ck,cs)
-        #API申請で取得したキー入れる
+        # API申請で取得したキー入れる
         auth.set_access_token(access_token, access_token_secret) 
         api = tweepy.API(auth, wait_on_rate_limit=True) #認証
         itr = tweepy.Cursor(api.followers_ids, id=screen_name, cursor=cursor).pages()
@@ -33,15 +31,16 @@ def get_follower(ck,cs):
                 user_info = {}
                 for user_info_key in user_info_keys:
                     user_info[user_info_key] = user._json[user_info_key]
-                user_info_list.append(user_info)            #user_info_listにuser_infoを入れ込む
+                user_info_list.append(user_info)            # user_info_listにuser_infoを入れ込む
             except tweepy.error.TweepError as e:
-                result = {"status_code":400}
+                result = {'status_code':400 , 'message':'Error'}
                 result = json.dumps(result)
                 return result
             except ConnectionError as e:
-                result = {"status_code":500}
+                result = {'status_code':500 , 'message':'Error'}
                 result = json.dumps(result)
                 return result
-        result = {"status_code":200,"context":user_info_list}
+        result = {'status_code':200 , 'message':'OK' , 'context':user_info_list}
         result = json.dumps(result)
         return result
+    
