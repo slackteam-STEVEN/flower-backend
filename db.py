1f960e807973837c77ec374d8f1a5ee13ac4e345
take-tech <57340643+take-tech@users.noreply.github.com>
2020-04-30 20:57:29 +0900

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ def conn_f():
     con = MySQLdb.connect(
         user    = 'root',       # 'your username',
         passwd  = 'Flower0112', # 'your password',
-        host    = '10.0.1.10',  # 'localhost',
+        host    = '10.0.2.10',  # 'localhost',
         port    = 3306,
         db      = 'flower', 
         charset = 'utf8'
